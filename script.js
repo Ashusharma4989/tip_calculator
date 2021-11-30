12cf43b7e6921cd24722266d9c62156e78581f7a
Ashusharma4989 <94176238+Ashusharma4989@users.noreply.github.com>
2021-11-30 16:52:53 +0530

    w

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const tipcalcy=()=>{
     let amount=document.getElementById('bill_amount').value;
     let perc=document.getElementById('tip_percent').value;
     let tip=amount*(perc/100);
-    let total_bill=tip+amount;
-    document.getElementById('tip_amount').value=tip;
-    document.getElementById('total_bill').value=total_bill;
+    let person=document.getElementById('bill_person').value;
+    let tipforeach=tip/person;
+    document.getElementById('total_bill').value=tipforeach;
+    
 }
